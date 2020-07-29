349d55b6640c4b1d08269cc7f32f70bddd434743
Mobile Dev <mobiledev0702@hotmail.com>
2020-07-29 12:11:25 +0800

    initial commit

---
+++ app/src/main/java/com/itspread/stardropletsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.itspread.stardropletsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
